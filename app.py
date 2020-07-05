efaa98337d71432483d59e256b84ad1c0e779eb6
huangliang <694845153@qq.com>
2020-07-06 01:48:07 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import click
-from flask import Flask,url_for,request,render_template
+from flask import Flask,url_for,request,render_template,flash,redirect
 from flask_sqlalchemy import SQLAlchemy
 
 WIN = sys.platform.startswith("win")
@@ -10,6 +10,7 @@ prefix = 'sqlite:///' if WIN else 'sqlite:////'
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'dev'
 db = SQLAlchemy(app)
 
 @app.cli.command()
@@ -19,39 +20,6 @@ def initdb(drop):
         db.drop_all()
     db.create_all()
     click.echo('Initialized	database.')
-
-class User(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(20))
-
-class Movie(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.String(20))
-    year = db.Column(db.String(20))
-
-
-
-# @app.route('/')
-# def hello():
-#     return 'Hello World!'
-
-
-@app.context_processor
-def inject_user():
-    user = User.query.first()
-    return dict(user=user)
-
-@app.errorhandler(404)
-def page_not_found(e):
-    user = User.query.first()
-    return render_template('404.html'), 404
-
-@app.route('/index')
-def index():
-    user = User.query.first()
-    movies = Movie.query.all()
-    return render_template('index.html', movies=movies)
-
 
 @app.cli.command()
 def forge():
@@ -78,6 +46,76 @@ def forge():
     db.session.commit()
     click.echo('Dnoe')
 
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(20))
+    year = db.Column(db.String(20))
+
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        # get worksheet data
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not year or len(year) > 4 or len(title) > 30:
+            flash('Invalid input.')
+            return redirect(url_for('index'))
+
+        movie = Movie(title=title,year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
+
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
+
+@app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) > 4 or len(title) > 30:
+            flash('Invalid input.')
+            return redirect(url_for('edit',movie_id=movie_id))
+
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Item updated.')
+        return redirect(url_for('index'))
+
+    return render_template('edit.html',movie=movie)
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index'))  # 重定向回主页
+
+
+if __name__ == '__main__':
+    app.run()
+
+
 # @app.route('/user/<name>')
 # def user_page(name):
 #     return '<h1>User Page</h1> %s' % name
@@ -91,16 +129,7 @@ def forge():
 # def test_url_for():
 #     print (url_for('hello'))
 #     print(url_for('user_page', name='gremlin'))
-#     print(url_for('user_page', name='peter'))
 #     print(url_for('test_url_for'))
 #     print(url_for('test_url_for', num=2))
 #     return 'Test Page'
-
-
-
-
-
-
-if __name__ == '__main__':
-    app.run()
 
